3b9a0d9df11f53acb6b3382babdfcbe9cac62d82
Mark Dalton <mark2813@hotmail.com>
2018-12-09 16:40:20 -0500

    feed

--- feed.php
+++ feed.php
@@ -1,6 +1,24 @@
 <?php
+$title = "Your Feed";
+$page = "feed";
 
+$content = <<<EOT
+	<div class='row feed-info'>
+		<div class='col-2 feed-types'>
+			<ul class="list-group list-group-flush">
+			  <li class="list-group-item">
+			  	<a href='#'> Your Feed </a>
+			  </li>
+			  <li class="list-group-item">
+			  	<a href='#'> Website Feed </a>
+			  </li>
+			</ul>
+		</div>
+		<div class='col-10'>
+			POSTS
+		</div>
+	</div>
+EOT;
+
+include 'template.php';
 ?>
-<div id="feed">
-    
-</div>
